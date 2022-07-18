f9150798a1a02618cc6080de0789b94875593b7a
Tiziana A. Gelmi Candusso <tiziana.gelmicandusso@utoronto.ca>
2022-07-18 13:12:36 -0400

    update detection_matrix_revsites_15072022

--- multi-species occupancy.R
+++ multi-species occupancy.R
@@ -50,8 +50,10 @@ b2000 <- left_join(b2000, human_dog_df, by="site_name")%>%
 
 ##call occupancy covariates
 cov<- b1000 %>% select(-1, -BUFF_DIST, -SHAPE_Length, -ORIG_FID, -SHAPE_Area)
-cov <- as.data.frame(scale(cov))
-
+cov<- b1000 %>% select(-1, -BUFF_DIST, -SHAPE_Length, -ORIG_FID, -SHAPE_Area)
+cov1 <- cov[,1:2]
+cov2 <- as.data.frame(scale(cov[,3:ncol(cov)]))
+cov<- cbind(cov1,cov2)
 ##call detection covariate matrix here if using
 ##det_list <- list(season = det_covs)
 
